92125814e3a4480e60ec2d71656b017d32799a0c
Artem Belousov <zopash1@gmail.com>
2022-08-01 19:28:26 +0300

    add gulp and webpack

---
+++ src/js/modules/toDoList.js
@@ -0,0 +1,68 @@
+function toDoList() {
+    const todoList = document.querySelector(".todolist__list"),
+          pencil = document.querySelector('[data-openAdd]'),
+          addText = document.querySelector('.todolist__add'),
+          btnSave = document.querySelector('[data-save]'),
+          btnClear = document.querySelector('[data-clear]'),
+          btnTips = document.querySelector('[data-tips]'),
+          tips = document.querySelector('.todolist__tips');
+    
+    function addToDo(e) {
+        if(e.code === 'Enter') {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `
+            <span data-close class="icon-trash-can-solid"></span>${e.target.value}
+            `;
+            todoList.append(listItem);
+            e.target.value = '';
+        }
+    }
+    
+    function checked(elem) {
+        if(elem.tagName === "LI") {
+            elem.classList.toggle('checked');
+        }
+    }
+    
+    function removeToDo(elem) {
+        if(elem.hasAttribute('data-close')) {
+            elem.parentNode.remove();
+            saveTodo();
+        }
+    }
+
+    function saveTodo() {
+         localStorage.setItem('todoList', todoList.innerHTML);
+    }
+
+    function loadTodo() {
+        const data = localStorage.getItem('todoList');
+        if(data) {
+            todoList.innerHTML = data;
+        }
+    }
+    loadTodo();
+
+    todoList.addEventListener('click', (e) => {
+        const target = e.target;
+        removeToDo(target);
+        checked(target);
+        console.dir(target);
+    });
+
+    addText.addEventListener('keydown', addToDo);
+    btnSave.addEventListener('click', saveTodo);
+    pencil.addEventListener('click', () => {
+        addText.classList.toggle('hide');
+    });
+    btnClear.addEventListener('click', () => {
+        todoList.innerHTML = '';
+        localStorage.clear();
+    });
+    btnTips.addEventListener('click', () => {
+        tips.classList.toggle('show');
+    });
+}
+
+export {toDoList};
+
